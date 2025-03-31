d67eb2fe39b3617e5270a9ae6364193e43085849
OmniVisionAnalytics <andrew.ra@yahoo.com>
2025-03-31 13:35:20 -0400

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
-  basePath: '', // or '' if you're deploying to a user/organization page
-};
-
-module.exports = nextConfig;
