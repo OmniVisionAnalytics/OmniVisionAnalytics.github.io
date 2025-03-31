37c41ccef43371bcd8bde3a80e4d7a046bc0132b
OmniVisionAnalytics <andrew.ra@yahoo.com>
2025-03-31 13:21:17 -0400

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,6 @@
+// next.config.js
+module.exports = {
+  output: 'export',
+  images: { unoptimized: true }, // needed for GitHub Pages
+  basePath: '/omnivision-analytics', // match your GitHub repo name
+};
